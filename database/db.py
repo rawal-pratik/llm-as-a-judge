68537e8485383c4a51887526e1048b2c27aa2c73
Pratik Rawal <pratikrawal40@gmail.com>
2026-06-29 08:20:43 +0000

    Database changed

--- database/db.py
+++ database/db.py
@@ -6,7 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# SQLite needs check_same_thread=False; Postgres/others do not accept it.
+_connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=_connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
